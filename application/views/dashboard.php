379933db6d5be427c2cc13c27fbe725a06d5a37c
larasamelia31 <larasamelia04@gmail.com>
2023-01-06 00:29:56 +0700

    laras: membuat detail keranjang, hapus keranjang dan pembayaran

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -11,7 +11,7 @@
   </div>
 </div>
 
-    <div class="row text-center mt-3">
+    <div class = "row text-center mt-3">
 
     <?php foreach ($barang as $brg) : ?>
 
@@ -20,8 +20,10 @@
             <div class="card-body">
             <h5 class="card-title mb-1"><?php echo $brg->nama_brg ?></h5>
             <small><?php echo $brg->keterangan ?></small><br>
-            <span class="badge badge-pill badge-success mb-3">Rp. <?php echo $brg->harga ?></span>
-            <a href="#" class="btn btn-sm btn-primary">Tambah ke Keranjang</a>
+            <span class="badge badge-pill badge-success mb-3">Rp.
+            <?php echo  number_format($brg->harga, 0, ',', '.') ?></span>
+            <?php echo anchor('dashboard/tambah_ke_keranjang/'.$brg
+              ->id_brg,'<div class="btn btn-sm btn-primary">Tambah ke Keranjang</div>') ?>
             <a href="#" class="btn btn-sm btn-success">Detail</a>
             </div>
         </div>
