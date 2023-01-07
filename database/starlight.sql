3f830c50200a54008f5341117c6ffa451209e98a
azrdksn123@gmail.com <azrdneperone@gmail.com>
2023-01-07 20:57:44 +0700

    master : upload laporan sesi 2

--- database/starlight.sql
+++ database/starlight.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3307
--- Generation Time: Jan 05, 2023 at 06:28 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 7.4.33
+-- Host: 127.0.0.1
+-- Generation Time: Jan 07, 2023 at 02:45 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -35,7 +36,7 @@ CREATE TABLE `tb_barang` (
   `harga` int(11) NOT NULL,
   `stok` int(4) NOT NULL,
   `gambar` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tb_barang`
@@ -45,7 +46,8 @@ INSERT INTO `tb_barang` (`id_barang`, `nama_brg`, `keterangan`, `kategori`, `har
 (1, 'Keripik Pisang Chips', 'Pisang diiris tipis kemudian digoreng dan dibumbui', 'Makanan', 25000, 15, 'keripikpisangchip.jpg'),
 (2, 'Dodol Duren', 'Lempok yang dibuat dengan bahan durian', 'Makanan', 18000, 10, 'dodolduren.jpg'),
 (3, 'Tokyo Banana', 'Kue yang di dalamnya diisi dengan krim pisang', 'Makanan', 11000, 8, 'tokyobanana.jpg'),
-(4, 'Pie Pisang Besar', 'Pie yang dicetak berbentuk pisang', 'Makanan', 50000, 6, 'piepisangbesar.jpg');
+(4, 'Pie Pisang Besar', 'Pie yang dicetak berbentuk pisang', 'Makanan', 50000, 6, 'piepisangbesar.jpg'),
+(5, 'Kopi', 'Americano', 'Minuman', 20000, 35, 'americano.jpg');
 
 -- --------------------------------------------------------
 
@@ -59,7 +61,14 @@ CREATE TABLE `tb_invoice` (
   `alamat` varchar(225) NOT NULL,
   `tgl_pesan` datetime NOT NULL,
   `batas_bayar` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_invoice`
+--
+
+INSERT INTO `tb_invoice` (`id`, `nama`, `alamat`, `tgl_pesan`, `batas_bayar`) VALUES
+(4, 'LAILATUN NI\'MAH', 'Dsn.Sindangrasa', '2023-01-07 12:50:26', '2023-01-08 12:50:26');
 
 -- --------------------------------------------------------
 
@@ -75,7 +84,14 @@ CREATE TABLE `tb_pesanan` (
   `jumlah` int(3) NOT NULL,
   `harga` int(10) NOT NULL,
   `pilihan` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_pesanan`
+--
+
+INSERT INTO `tb_pesanan` (`id`, `id_invoice`, `id_barang`, `nama_brg`, `jumlah`, `harga`, `pilihan`) VALUES
+(2, 4, 4, 'Pie Pisang Besar', 1, 50000, '');
 
 --
 -- Indexes for dumped tables
@@ -107,19 +123,19 @@ ALTER TABLE `tb_pesanan`
 -- AUTO_INCREMENT for table `tb_barang`
 --
 ALTER TABLE `tb_barang`
-  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tb_invoice`
 --
 ALTER TABLE `tb_invoice`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tb_pesanan`
 --
 ALTER TABLE `tb_pesanan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
